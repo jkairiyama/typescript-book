2f055befc58cc5ed024235212b0a70597e7ece07
jkairiyama <shochorii@gmail.com>
2019-07-27 10:19:24 -0300

    test 4

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,45 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   console.log('Hello Gulp!');
 });
+
+var tslint = require('gulp-tslint');
+
+gulp.task('lint', function() {
+  return gulp.src(['./source/ts/**/**.ts', './test/**/**.test.ts']).
+     pipe(tslint()).
+     pipe(tslint.report('verbose'));
+});
+
+//gulp.task('default', ['lint']);
+
+var ts = require('gulp-typescript');
+
+var tsProject = ts.createProject({
+  removeComments : true,
+  noImplicitAny : true,
+  target : 'ES3',
+  module : 'commonjs',
+  declarationFiles : false
+});
+
+gulp.task('tsc', function() {
+return gulp.src('./source/ts/**/**.ts')
+.pipe(ts(tsProject))
+.js.pipe(gulp.dest('./temp/source/js'));
+});
+
+var tsTestProject = ts.createProject({
+removeComments : true,
+noImplicitAny : true,
+target : 'ES3',
+module : 'commonjs',
+declarationFiles : false
+});
+
+gulp.task('tsc-tests', function() {
+return gulp.src('./test/**/**.test.ts')
+.pipe(ts(tsTestProject ))
+.js.pipe(gulp.dest('./temp/test/'));
+});
+
+gulp.task('default', ['lint', 'tsc', 'tsc-tests']);
